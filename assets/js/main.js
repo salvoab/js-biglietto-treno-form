e969094530aee7eea1f2dd12ee3a869c954a1657
salvoab <salvatore.abate85@gmail.com>
2020-11-16 16:41:39 +0100

    Output su pagina html

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,21 +10,34 @@ btnGenera.addEventListener("click", function(){
     if( nomePasseggero != "" && kmViaggio > 0 ){
         var prezzoAlKm = 0.21;
         var prezzoBiglietto = kmViaggio * prezzoAlKm;
-        console.log("Prezzo del biglietto: " + prezzoBiglietto + "€");
+        var tipoOfferta = "Prezzo Standard";
         
         //Verifico se è necessario applicare uno sconto
         var sconto = 0;
         if(fasciaEta == "minorenne" ){
             //Passeggero minorenne e applico lo sconto del 20%
             sconto = prezzoBiglietto * 20 / 100;
-            console.log("Sconto del 20%: " + sconto);
+            tipoOfferta = "Sconto Minorenne";
         } else if(fasciaEta == "over65"){
             sconto = prezzoBiglietto * 40 / 100;
-            console.log("Sconto del 40%: " + sconto);
+            tipoOfferta = "Sconto Senior";
         }
         //Sottraggo lo sconto al prezzoBiglietto (se lo sconto non è stato applicato sottraggo 0)
         prezzoBiglietto = prezzoBiglietto - sconto;
-        console.log("Il prezzo finale del biglietto è: " + prezzoBiglietto.toFixed(2));
+        //Selezione elementi di output
+        var elementoNomePasseggero = document.getElementById("nome-passeggero");
+        var elementoOfferta = document.getElementById("offerta");
+        var elementoCarrozza = document.getElementById("carrozza");
+        var elementoCodiceCp = document.getElementById("codice-cp");
+        var elementoPrezzo = document.getElementById("prezzo-biglietto");
+        //Output
+        elementoNomePasseggero.innerHTML = nomePasseggero;
+        elementoOfferta.innerHTML = tipoOfferta;
+        //Carrozza è un numero random da 1 a 10
+        elementoCarrozza.innerHTML = Math.floor(Math.random() * 10) + 1;
+        //Codice Cp è un numero compreso fra 90000 e 100000
+        elementoCodiceCp.innerHTML = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+        elementoPrezzo.innerHTML = prezzoBiglietto.toFixed(2) + "€";
     }
     else{
         alert("Errore! E' obbligatorio inserire Nome e Cognome e i km devono essere più di zero");
